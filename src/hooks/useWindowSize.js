cd50d21f988a09bed9c3033d71bd2b6a56f90b6b
Mikael B <borges.mikael@gmail.com>
2023-08-01 16:20:57 +0200

    minor improvements

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -3,9 +3,7 @@ import { useLayoutEffect, useState } from "react";
 export function useWindowSize() {
   const [size, setSize] = useState([0, 0]);
   useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
+    const updateSize = () => setSize([window.innerWidth, window.innerHeight]);
     window.addEventListener("resize", updateSize);
     updateSize();
     return () => window.removeEventListener("resize", updateSize);
